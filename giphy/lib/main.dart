9a3ca18a2c4b4fcf0afce21a6e811e54a076432c
Luann Luna <luann.marques@gmail.com>
2019-06-17 22:28:16 -0300

    Finalizado App buscador de Gif's

---
+++ giphy/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:giphy/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
